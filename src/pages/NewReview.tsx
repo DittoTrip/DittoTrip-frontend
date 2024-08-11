aadc7af8014d5ce4b14e769b471554166cf1df08
bsu0404 <bsu0404@naver.com>
2024-08-12 00:00:09 +0900

    Feat: 리뷰생성 페이지

---
+++ src/pages/NewReview.tsx
@@ -0,0 +1,9 @@
+import { styled } from 'styled-components';
+
+const NewReview = () => {
+  return <NewReviewStyle></NewReviewStyle>;
+};
+
+const NewReviewStyle = styled.div``;
+
+export default NewReview;
