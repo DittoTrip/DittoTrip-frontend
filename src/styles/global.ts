9f4145c302955135314537c9ab29fe4a41810bed
bsu0404 <bsu0404@naver.com>
2024-08-13 18:34:17 +0900

    Fix: 폰트 설정, background 색상 설정

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,3 @@
-import 'sanitize.css';
 import { createGlobalStyle } from 'styled-components';
 import { ThemeName } from './theme';
 
@@ -6,16 +5,20 @@ interface Props {
   themeName: ThemeName;
 }
 export const GlobalStyle = createGlobalStyle<Props>`
+
+    :root{
+        background-color: ${props => (props.themeName === 'light' ? 'white' : 'black')}
+    }
+    
     body{
         padding: 0;
         margin: 0;
-        background-color: ${props => (props.themeName === 'light' ? 'white' : 'black')}
     }
     h1{
         margin: 0;
     }
     *{
-        font-family : 'Noto Sans KR';
+        font-family : 'Noto Sans KR', sans-serif;
         color: ${props => (props.themeName === 'light' ? 'black' : 'white')};
     }
 `;
