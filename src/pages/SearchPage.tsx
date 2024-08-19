49a859852ddacdf852c9e85c9249b5f87af49745
Tony <gogo6570@gmail.com>
2024-08-19 13:11:35 +0900

    Fix: 검색 페이지 아이콘 수정

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -2,17 +2,34 @@ import styled from "styled-components"
 import { useTranslation } from "react-i18next";
 import { useState } from "react";
 import SearchBar from "../components/common/SearchBar";
-
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 const SearchPage = () => {
     const { t } = useTranslation();
     const [searchWord, setSearchWord] = useState('');
+    let navigate = useNavigate();
     return (
         <SearchPageStyled>
+            
             <div className="title">무엇을 찾으시나요?</div>
             <SearchBar setSearchWord={setSearchWord} placeHolder={t('search.placeHolder')} />
 
+            <div className="recent">
+                <div className="recent-title">{t('search.recentSearches')}</div>
+                <div className="delete-btn">
+                    <FontAwesomeIcon icon={faXmark}/>
+                </div>
+                <div className="delete-btn">
+                    <FontAwesomeIcon icon={faXmark}/>
+                </div>
+                <div className="delete-btn">
+                    <FontAwesomeIcon icon={faXmark}/>
+                </div>
+            </div>
+
+            
         </SearchPageStyled>
     )
 }
