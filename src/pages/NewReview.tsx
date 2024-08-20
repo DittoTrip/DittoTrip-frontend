a16f32722575a01b1a9618b5156c99fd469449a2
bsu0404 <bsu0404@naver.com>
2024-08-20 20:12:55 +0900

    Feat: 새 리뷰 작성하기 ui

--- src/pages/NewReview.tsx
+++ src/pages/NewReview.tsx
@@ -1,21 +1,38 @@
 import { styled } from 'styled-components';
+import { useTranslation } from 'react-i18next';
 
-import LangSelectButton from '../components/LangSelectButton';
 import AppBar from '../components/common/AppBar';
+import LangSelectButton from '../components/LangSelectButton';
 import ImageUploader from '../components/review/UploadImage';
+import Star from '../components/common/Star';
+import Button from '../components/common/Button';
 
 const NewReview = () => {
+  const { t } = useTranslation();
+
   return (
     <NewReviewStyle>
       <div className="app-bar">
         <AppBar leading={true} title={<div className="title">리뷰 쓰기</div>} action={<LangSelectButton />} />
       </div>
-      <div>리뷰</div>
-      <div>
-        <input type="text" className="review-text" placeholder="1000자까지 작성가능합니다." />
+      <div className="location-rating-card">
+        <div className="spot-name">촬영지 이름</div>
+        <Star rating={4.5} showRatingValue={false} color="keyColor" gap={12} size={28} />
       </div>
-      <div>포토</div>
-      <ImageUploader />
+
+      <div className="review-input-box">
+        <div>리뷰</div>
+        <input type="text" className="review-text" placeholder={t('')} />
+
+        <div>포토</div>
+        <ImageUploader />
+      </div>
+
+      <div className="review-submit">
+        <Button size="large" scheme="subButton">
+          작성 완료
+        </Button>
+      </div>
     </NewReviewStyle>
   );
 };
@@ -24,17 +41,46 @@ const NewReviewStyle = styled.div`
   .app-bar .title {
     display: flex;
     justify-content: start;
+
     ${({ theme }) => theme.font.subTitle};
   }
-  .review-text {
-    height: 200px;
+
+  .location-rating-card {
+    padding-top: 12px;
+    padding-bottom: 24px;
+
+    border-bottom: 1px solid ${({ theme }) => theme.color.gray20};
+
+    .spot-name {
+      text-align: center;
+      margin-bottom: 8px;
+      ${({ theme }) => theme.font.body2}
+    }
+  }
+
+  .review-input-box {
+    padding: 0 28px;
+
+    .review-text {
+      height: 200px;
+      width: 100%;
+
+      border-radius: 12px;
+      border: none;
+      outline: none;
+
+      background-color: ${({ theme }) => theme.color.gray20};
+    }
+  }
+
+  .review-submit {
+    position: fixed;
+    bottom: 87px;
+    left: 0;
+
     width: 100%;
 
-    border-radius: 12px;
-    border: none;
-    outline: none;
-
-    background-color: ${({ theme }) => theme.color.gray20};
+    padding: 0 28px;
   }
 `;
 
