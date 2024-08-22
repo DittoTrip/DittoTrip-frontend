1bf858ff17f9139a212792bc304f29cda52792a8
Tony <gogo6570@gmail.com>
2024-08-23 07:43:08 +0900

    Feat: 검색 페이지 개발

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -5,6 +5,18 @@ import SearchBar from "../components/common/SearchBar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
+import LangSelectButton from "../components/LangSelectButton";
+import AppBar from "../components/common/AppBar";
+import TextSlide from "../components/common/TextSlide";
+
+export interface searchItem {
+    title : string;
+}
+
+const CAROUSEL_TEXTS = [
+    { title : "도꺠비"},{ title : "공유"},{ title : "강원도"},
+    { title : "이상한 변호사 우영우"},{ title : "도꺠비"},{ title : "도꺠비"},
+]
 
 const Search = () => {
     const { t } = useTranslation();
@@ -12,33 +24,73 @@ const Search = () => {
     let navigate = useNavigate();
     return (
         <SearchStyled>
-            
-            <div className="title">무엇을 찾으시나요?</div>
-            <SearchBar setSearchWord={setSearchWord} placeholder={t('search.placeHolder')} />
-
-            <div className="recent">
-                <div className="recent-title">{t('search.recentSearches')}</div>
-                <div className="delete-btn">
-                    <FontAwesomeIcon icon={faXmark}/>
-                </div>
-                <div className="delete-btn">
-                    <FontAwesomeIcon icon={faXmark}/>
-                </div>
-                <div className="delete-btn">
-                    <FontAwesomeIcon icon={faXmark}/>
-                </div>
+            <div className="app-bar">
+                <AppBar
+                    leading={true}
+                    title={
+                        <div className="title">
+                            무엇을 찾으시나요?
+                        </div>
+                }
+                action={<LangSelectButton/>}
+                />
             </div>
+            
+            <div className="container">
+            <SearchBar setSearchWord={setSearchWord} placeholder={t('search.placeHolder')} />
+                
+                <div className="search-title">최근 검색</div>
 
+                <div className="recent-list">
+                    <div className="recent-content">슬기로운 깜빵생활</div>
+                    <div className="delete-btn">
+                    <FontAwesomeIcon icon={faXmark}/>
+                    </div>
+                </div>
+                <div className="clear"/>
+                
+                <div className="search-title"> 추천 검색어</div>
+                <TextSlide carouselList={CAROUSEL_TEXTS}/>
+                
+                <div className="search-title">급상승 디토</div>
+                
+            </div>
             
         </SearchStyled>
     )
 }
 
 const SearchStyled = styled.div`
-    .title {
-        color: ${({theme}) => theme.color.keyColor};
-        ${({theme})=> theme.font.subTitle}
+    .app-bar .title {
+    display: flex;
+    justify-content: center;
+    ${({ theme }) => theme.font.body1};
+    color: ${({theme})=>theme.color.keyColor};
     }
+    .search-title {
+        margin-top: 32px;
+        margin-bottom: 16px;
+        ${({theme})=>theme.font.body2};
+    }
+    .container {
+        margin: 0 27px;
+    }
+    .recent-list {
+        display: block;
+    }
+    .recent-content{
+        width: 95%;
+        float: left;
+    }
+    .delete-btn{
+        width: 5%;
+        float: left;
+    }
+    .clear {
+        clear: both;
+    }
+
+
 `
 
 export default Search;
