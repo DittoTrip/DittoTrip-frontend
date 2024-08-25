dc88b73803fcb6c81e46007fc7fa27ca5bde58cc
bsu0404 <bsu0404@naver.com>
2024-08-25 14:19:13 +0900

    Feat: 버튼 수정

--- src/pages/Join.tsx
+++ src/pages/Join.tsx
@@ -6,14 +6,15 @@ import Button from '../components/common/Button';
 type Inputs = {
   nickname: string;
   email: string;
+  checkEmail: string;
   password: string;
+  checkPassword: string;
 };
 
 const Join = () => {
   const {
     register,
     handleSubmit,
-
     formState: { errors },
   } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
@@ -29,8 +30,11 @@ const Join = () => {
             <div className="input-title">닉네임</div>
             <div className="user-varification">
               <input {...register('nickname')} placeholder="닉네임" className="input-container" />
+              <Button size={'medium'} scheme={'keyButton'}>
+                중복 확인
+              </Button>
             </div>
-            {errors.email && <span>닉네임을 입력해주세요</span>}
+            {errors.email && <span className="guide-message">닉네임을 입력해주세요</span>}
             <div className="error-message">이미 사용 중인 닉네임입니다.</div>
           </div>
           <div className="section">
@@ -43,18 +47,18 @@ const Join = () => {
                 className="input-container"
               />
               <Button size={'medium'} scheme={'keyButton'}>
-                중복 확인
+                인증하기
               </Button>
             </div>
             <div className="user-varification">
               <input
-                {...register('email', { required: true })}
+                {...register('checkEmail', { required: true })}
                 placeholder="example@gmail.com"
                 type="email"
                 className="input-container"
               />
               <Button size={'medium'} scheme={'keyButton'}>
-                인증하기
+                확인
               </Button>
             </div>
             {errors.email && <span className="guide-message">이메일을 입력해주세요</span>}
@@ -74,12 +78,12 @@ const Join = () => {
           </div>
           <div className="section">
             <input
-              {...register('password', { required: true })}
+              {...register('checkPassword', { required: true })}
               placeholder="check password"
               type="password"
               className="input-container"
             />
-            {errors.password && <span className="guide-message">비밀번호확인을 입력해주세요</span>}
+            {errors.password && <span className="guide-message">비밀번호 확인을 입력해주세요</span>}
             <div className="error-message">
               영문 대문자와 소문자, 숫자, 특수문자 중 2가지 이상을 조합하여 10~16자로 입력해 주세요.
             </div>
@@ -102,39 +106,46 @@ const JoinStyle = styled.div`
     }
   }
   .content-wrapper {
-    margin: 0 28px;
+    margin: 30px 28px 0 28px;
 
     .section {
       .input-title {
+        margin-bottom: 10px;
+
         color: ${({ theme }) => theme.color.gray80};
         ${({ theme }) => theme.font.body2};
-        margin-bottom: 10px;
       }
       .user-varification {
         display: flex;
-        gap: 13px;
         align-items: center;
         justify-content: space-between;
+        gap: 13px;
+
         margin-bottom: 10px;
       }
       .error-message {
-        color: ${({ theme }) => theme.color.red};
         margin-bottom: 10px;
+
+        color: ${({ theme }) => theme.color.red};
+        ${({ theme }) => theme.font.body5};
       }
     }
   }
 
   .input-container {
-    flex: 1;
-    border: none;
-    border-radius: 12px;
+    width: 100%;
     height: 52px;
     flex: 1;
-    width: 100%;
+
+    border: none;
+    border-radius: 12px;
     background: ${({ theme }) => theme.color.gray20};
   }
   .guide-message {
+    margin-bottom: 10px;
+
     color: ${({ theme }) => theme.color.red};
+    ${({ theme }) => theme.font.body5};
   }
 `;
 export default Join;
