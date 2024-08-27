18cef1acf96d083cb998838d5fbe01719cd6b394
Tony <gogo6570@gmail.com>
2024-08-27 23:24:30 +0900

    Feat: 검색 결과 페이지 개발중

---
+++ src/pages/SearchResult.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+const SearchResult = () => {
+    return (
+        <SearchResultStyled>
+            
+        </SearchResultStyled>
+    )
+}
+
+const SearchResultStyled = styled.div``
+
+export default SearchResult;
