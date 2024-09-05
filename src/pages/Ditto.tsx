28342bc1d3b545c3c3c737fd7402f668b767a25b
Tony <gogo6570@gmail.com>
2024-09-06 03:10:25 +0900

    Feat : 디토 메인페이지 컴포넌트화

--- src/pages/Ditto.tsx
+++ src/pages/Ditto.tsx
@@ -4,6 +4,13 @@ import LangSelectButton from "../components/LangSelectButton";
 import AppBar from "../components/common/AppBar";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import DittoInfinity from "../components/ditto/DittoInfinity";
+
+export interface dittoInfi {
+    img: string;
+    title : string;
+    name : string;
+}
 
 const DummyDitto = [
     {
@@ -64,20 +71,7 @@ const Ditto = () => {
             <div className="search-bar">
                 <SearchBar setSearchWord={setSearchWord} placeholder={t('search.placeholder')} />
             </div>
-            <div className="content-wrapper">
-                {DummyDitto.map((item, i)=>{
-                    return (
-                        <div key={i} className="ditto-box">
-                            <img className="content-img" src={item.img}></img>
-                            <div className="title-box">
-                                <div className="location">{item.title}</div>
-                                <div className="name">{item.name}</div>
-                            </div>
-                        </div>
-                    )
-                })}
-            </div>
-            
+            <DittoInfinity dittoList={DummyDitto}/> 
         </DittoStyle>
     )
 }
@@ -90,52 +84,6 @@ const DittoStyle = styled.div`
     .search-bar {
         margin: 8px 28px;
         margin-bottom: 13px;
-    }
-    .content-wrapper {
-        margin: 8px 28px;
-        display: grid;
-        grid-template-columns: repeat(2, 1fr); 
-        gap: 8px; 
-    }
-
-    .ditto-box {
-        position: relative;
-        width: 100%; 
-        display: block;
-    }
-
-    .content-img {
-        border-radius: 15px;
-        width: 100%; 
-        height: auto; 
-        display: block;
-    }
-
-    .title-box {
-        position: absolute;
-        bottom: 10px;
-        left: 10px;
-        
-        .location {
-            ${({theme}) => theme.font.body2}
-            color: white;
-        }
-        .name {
-            ${({theme}) => theme.font.body5}
-            color: white;
-        }
-    }
-
-    @media (max-width: 768px) {
-        .content-wrapper {
-            grid-template-columns: repeat(2, 1fr); 
-        }
-    }
-
-    @media (max-width: 300px) {
-        .content-wrapper {
-            grid-template-columns: repeat(1, 1fr); /* 아주 작은 화면에서는 1줄로 변경 */
-        }
     }
 `;
 
